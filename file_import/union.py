252ea2450a0b913b91bb22b37ab378dfcb1eb34d
Israel <israelias94@gmail.com>
2023-08-05 11:05:32 -0300

    feat: union and bisa scripts updated

--- file_import/union.py
+++ file_import/union.py
@@ -13,17 +13,21 @@ ruta_archivo = sys.argv[1]
 df = pd.read_excel(ruta_archivo, header=15)
 
 # Encuentra el índice de la fila que contiene "Total Créditos:"
-index_total_creditos = df[df['Fecha Movimiento'] == 'Total Créditos:'].index[0]
+try:
+    index_total_creditos = df[df["Fecha Movimiento"] == "Total Créditos:"].index[0]
+except IndexError:
+    index_total_creditos = df[df["Unnamed: 1"] == "Total Créditos:"].index[0]
+
 
 # Filtra los datos que deseas mantener
-df = df.iloc[:index_total_creditos-1]
-df = df[['Fecha Movimiento', 'Monto\n', 'Descripción']]
+df = df.iloc[: index_total_creditos - 1]
+df = df[["Fecha Movimiento", "Monto\n", "Descripción"]]
 # df['Outflow'] = df['Monto\n'].apply(lambda x: abs(float(x.replace(',', ''))) if float(x.replace(',', '')) < 0 else '')
 # df['Inflow'] = df['Monto\n'].apply(lambda x: float(x.replace(',', '')) if float(x.replace(',', '')) > 0 else '')
-df['Amount'] = df['Monto\n']
+df["Amount"] = df["Monto\n"]
 # df = df[['Fecha Movimiento', 'Outflow', 'Inflow', 'Descripción']]
-df = df[['Fecha Movimiento', 'Amount', 'Descripción']]
-df = df.rename(columns={'Fecha Movimiento': 'Date', 'Descripción': 'Memo'})
+df = df[["Fecha Movimiento", "Amount", "Descripción"]]
+df = df.rename(columns={"Fecha Movimiento": "Date", "Descripción": "Memo"})
 
 # Guarda el archivo CSV como "ynab.csv"
 df.to_csv("ynab.csv", index=False)
