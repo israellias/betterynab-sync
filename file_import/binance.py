4b79acc3de34dd20be3e35c6ae09524339a8fb41
Israel <israelias94@gmail.com>
2023-09-15 12:42:21 -0300

    feat: binance now generates a transaction for the commission applied to a trade

--- file_import/binance.py
+++ file_import/binance.py
@@ -12,54 +12,73 @@ if len(sys.argv) < 2:
 json_file = sys.argv[1]
 
 # Lee el archivo JSON
-with open(json_file, 'r') as file:
+with open(json_file, "r") as file:
     data = json.load(file)
 
 # Filtra los datos por orderStatus = 4
-filtered_data = [item for item in data['data'] if item['orderStatus'] == 4]
+filtered_data = [item for item in data["data"] if item["orderStatus"] == 4]
+
 
 # Función para obtener la fecha en formato dd/mm/yyyy a partir de un timestamp
 def get_formatted_date(timestamp):
     date = datetime.datetime.fromtimestamp(timestamp / 1000)
-    return date.strftime('%d/%m/%Y')
+    return date.strftime("%d/%m/%Y")
+
 
 # Función para redondear un número a dos decimales
 def round_number(num):
     return round(num, 2)
 
+
 # Función para formatear el monto con la cantidad de decimales adecuada según la moneda
 def format_amount(amount, fiat):
-    if fiat in ['USD', 'USDT']:
+    if fiat in ["USD", "USDT", "BOB"]:
         return round_number(amount)
     else:
         return int(amount)
 
+
 # Prepara los datos para el archivo CSV
 csv_data = []
 for item in filtered_data:
-    trade_type = item['tradeType']
-    amount = Decimal(item['amount'])
-    asset = item['asset']
-    total_price = Decimal(item['totalPrice'])
-    fiat = item['fiat']
-    username = item['buyerNickname'] if trade_type == 'SELL' else item['sellerNickname']
-    create_time = get_formatted_date(item['createTime'])
+    trade_type = item["tradeType"]
+    amount = Decimal(item["amount"])
+    asset = item["asset"]
+    total_price = Decimal(item["totalPrice"])
+    fiat = item["fiat"]
+    username = item["buyerNickname"] if trade_type == "SELL" else item["sellerNickname"]
+    create_time = get_formatted_date(item["createTime"])
+    commission = Decimal(item.get("commission", 0))  # Get commission or default to 0
+
     memo = f"{trade_type} {format_amount(amount, fiat)} {asset} with {format_amount(total_price, fiat)} {fiat} ({username})"
-    
-    if trade_type == 'BUY':
-        inflow = format_amount(amount, fiat)
-        outflow = ''
+
+    if trade_type == "BUY":
+        inflow = format_amount(total_price, fiat)
+        outflow = ""
     else:
-        inflow = ''
-        outflow = format_amount(amount, fiat)
-    
-    csv_data.append([create_time, outflow, inflow, memo])
+        inflow = ""
+        outflow = format_amount(amount - commission, fiat)
+
+    payee = "Transfer:Zinli" if trade_type == "BUY" else "Transfer:BOB"
+    csv_data.append([create_time, payee, outflow, inflow, memo])
+
+    if commission:
+        csv_data.append(
+            [
+                create_time,
+                "Commission to Zinli",
+                "",
+                format_amount(amount - total_price - commission, fiat),
+                f"Commission for {memo}",
+            ]
+        )
+
 
 # Escribe los datos en un archivo CSV
 csv_file = "ynab.csv"
-with open(csv_file, 'w', newline='') as file:
+with open(csv_file, "w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(['Date', 'Outflow', 'Inflow', 'Memo'])
+    writer.writerow(["Date", "Payee", "Outflow", "Inflow", "Memo"])
     writer.writerows(csv_data)
 
 print(f"Archivo CSV '{csv_file}' generado exitosamente.")
